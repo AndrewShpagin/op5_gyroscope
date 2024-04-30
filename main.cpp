3be62f77baab082c15008f48981104dc6702ac0a
AndrewShpagin <andrewshpagin@gmail.com>
2024-04-30 19:03:55 +0800

    100hz

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
     }
 
     DFRobot_WT61PC sensor(&FPSerial);
-    sensor.modifyFrequency(FREQUENCY_10HZ);
+    sensor.modifyFrequency(FREQUENCY_100HZ);
 
     do{
         if (sensor.available()) {
